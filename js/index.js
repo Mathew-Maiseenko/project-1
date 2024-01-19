3308b9ca641e91527724cd964da2170eb6598017
Mathew Maiseenko <maislov.alex@gmail.com>
2024-01-19 14:53:30 +0300

    Start commit

---
+++ js/index.js
@@ -0,0 +1,27 @@
+"use strict";
+alert("Я JavaScript!")
+let admin_1;
+let name_1;
+
+name_1 = "Джон";
+admin_1 = name_1;
+alert(admin_1);
+
+
+
+
+
+
+
+
+
+
+
+// const obj ={
+//     name: 'Mike',
+//     age: 25
+// };
+// const newPoint = 'placeOfWork';
+// obj.newPoint = "Front-end";
+// console.log(5);
+// console.log(obj.newPoint);
